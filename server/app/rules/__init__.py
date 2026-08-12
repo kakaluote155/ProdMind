9cd17257a6f02db4447b91d1660839579851d041
Ash <129935871+kakaluote155@users.noreply.github.com>
2026-08-12 13:58:57 +0800

    feat: register database pool exhaustion rule

--- server/app/rules/__init__.py
+++ server/app/rules/__init__.py
@@ -1,7 +1,9 @@
+from .capacity import DatabasePoolExhaustedRule
 from .database import DatabaseUniqueViolationRule
 from .network import DownstreamUnavailableRule
 
 RULES = [
+    DatabasePoolExhaustedRule(),
     DatabaseUniqueViolationRule(),
     DownstreamUnavailableRule(),
 ]
